a722aaf5cc244496e13ffaa31838df19c275b8a1
Shawn Jones <mryallups@gmail.com>
2019-05-19 15:50:37 -0700

    Fun state example with search box. Show increasing `...` while user leaves mouse over box

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,39 @@
 import React from 'react';
 
+const basePlaceholderText = 'Search';
 
-export default class Search extends React.Component {
+export default class Search extends React.PureComponent {
+  state = {
+    placeholderText: basePlaceholderText
+  };
+
+  handleOnHover = () => {
+    let postFix = '';
+
+    this.interval = setInterval(() => {
+      postFix += '.';
+      this.setState({
+        placeholderText: basePlaceholderText + postFix
+      });
+    }, 400);
+  };
+
+  handleOffHover = () => {
+    clearInterval(this.interval);
+    this.setState({
+      placeholderText: basePlaceholderText
+    });
+  };
+
   render() {
+    const { placeholderText } = this.state;
+
     return (
-      <input />
+      <input
+        placeholder={placeholderText}
+        onMouseEnter={this.handleOnHover}
+        onMouseLeave={this.handleOffHover}
+      />
     )
   }
 }
